d3015f38133521d4e2ae0f9e585ccacaf73d9408
sultan <zyldyzbekovsultan02@gmail.com>
2025-04-20 21:08:14 +0600

    Added the "addProperty" method to input property details and save them into file. The saving part was very hard at firts. Now it works normally.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,4 +40,39 @@ public class Main {
             System.out.println("No existing properties found");
         }
     }
+
+    static void addProperty() {
+        System.out.print("Enter ID: ");
+        String id = scanner.nextLine();
+
+        System.out.print("Enter title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Sale or Rent: ");
+        String type = scanner.nextLine();
+
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+
+        System.out.print("Price: ");
+        double price = Double.parseDouble(scanner.nextLine());
+
+        Property newProp = new Property(id, title, type, address, price);
+        properties.add(newProp);
+        saveProperties();
+        System.out.println("Property added!");
+    }
+
+    static void saveProperties() {
+        try {
+            PrintWriter writer = new PrintWriter(fileName);
+            for(int i = 0; i < properties.size(); i++) {
+                Property p = properties.get(i);
+                writer.println(p.id + "," + p.title + "," + p.type + "," + p.address + "," + p.price);
+            }
+            writer.close();
+        } catch(Exception e) {
+            System.out.println("Error saving data");
+        }
+    }
 }
